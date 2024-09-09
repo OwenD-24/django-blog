76ef0a93a9220c1a7b93e2fdc28152dd4be7c285
Owen Davis <owendavis1991@gmail.com>
2024-09-09 12:04:51 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0w3)8&65#)xbpf_n6(ee56pi^^ztm-04_76m_rq&pwx6tvf^n*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["8000-owend24-djangoblog-r1umyceemy7.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["django-blog244.herokuapp.com", "8000-owend24-djangoblog-r1umyceemy7.ws.codeinstitute-ide.net"]
 
 
 # Application definition
