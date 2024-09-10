b714d98263595a166cffb35af0aa27787decb24e
Owen Davis <owendavis1991@gmail.com>
2024-09-10 13:54:22 +0000

    Added environment variables configuration

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,14 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+# Secret Key
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Database
+DATABASE = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
