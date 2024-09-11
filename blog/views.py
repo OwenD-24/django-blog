8f0d36d678e4dd8ee396a45afea3cf3e3ae93ab3
Owen Davis <owendavis1991@gmail.com>
2024-09-11 23:12:47 +0000

    Added post list content

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-# Create your views here.
+# Create your views here
 class PostList(generic.ListView):
     model = Post
+    template_name = 'blog/post_list.html'
+
 
 
