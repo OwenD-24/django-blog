6d4e982c30b6f09b48dfbe7ab60f8755456e296e
Owen Davis <owendavis1991@gmail.com>
2024-09-12 12:57:07 +0000

    Added a filter option to the admin portal

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-owend24-djangoblog-r1umyceemy7.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
