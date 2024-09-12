974e600c97be3ccaae1e7bbe30cb1fa05a42bca1
Owen Davis <owendavis1991@gmail.com>
2024-09-12 13:24:17 +0000

    Added if os.path to settings.p for DEBUG true/false

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.path.isfile("env.py"):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ["8000-owend24-djangoblog-r1umyceemy7.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
