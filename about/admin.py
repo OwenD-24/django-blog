776c966030ab294c5fd9f96a468ab150596e9633
Owen Davis <owendavis1991@gmail.com>
2024-09-13 17:15:13 +0000

    Added URL for the About App and implemented a view function

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import About
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 admin.site.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title',)
+    summernote_fields = ('content',)
--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
 def about_view(request):
-    return render(request, 'about/about.html')
+    about_content = About.objects.all().first()
+    return render(request, 'about/about.html', {'about': about_content})
