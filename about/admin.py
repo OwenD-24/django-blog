96d40de030638c493561063bfb4c477dd21ae8ac
Owen Davis <owendavis1991@gmail.com>
2024-09-13 17:20:58 +0000

    Fixed any correctings while making the about app

--- about/admin.py
+++ about/admin.py
@@ -3,7 +3,7 @@ from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(About)
+@admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-    list_display = ('title',)
+
     summernote_fields = ('content',)
--- about/views.py
+++ about/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import About
 
 # Create your views here.
-def about_view(request):
-    about_content = About.objects.all().first()
-    return render(request, 'about/about.html', {'about': about_content})
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
